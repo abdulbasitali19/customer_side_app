8a6f3f5a1ee1ae813a69a3c982d4e8debff4e6fa
abdulbasitali19 <abdulbasitalishaik1@gmail.com>
2024-02-13 16:54:25 +0500

    unit of measure fields added

--- customer_side_app/hooks.py
+++ customer_side_app/hooks.py
@@ -31,7 +31,10 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+	"Stock Reconciliation" : "public/js/controllers/stock_reconciliation.js"
+}
+
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -187,4 +190,3 @@ user_data_fields = [
 # auth_hooks = [
 #	"customer_side_app.auth.validate"
 # ]
-
---
+++ customer_side_app/public/js/controllers/stock_reconciliation.js
@@ -0,0 +1,116 @@
+frappe.ui.form.on("Stock Reconciliation", {
+    refresh: function (frm) {
+        if (frm.doc.docstatus < 1) {
+            frm.add_custom_button(__("Fetch Items from Warehouse"), function () {
+                frm.events.get_items(frm);
+            });
+        }
+
+        if (frm.doc.company) {
+            frm.trigger("toggle_display_account_head");
+        }
+    },
+    get_items: function (frm) {
+        let fields = [
+            {
+                label: 'Warehouse',
+                fieldname: 'warehouse',
+                fieldtype: 'Link',
+                options: 'Warehouse',
+                reqd: 1,
+                "get_query": function () {
+                    return {
+                        "filters": {
+                            "company": frm.doc.company,
+                        }
+                    };
+                }
+            },
+            {
+                label: "Item Group",
+                fieldname: "item_group",
+                fieldtype: "Link",
+                options: "Item Group",
+                "get_query": function () {
+                    return {
+                        "filters": {
+                            "item_group": frm.doc.item_group,
+                        }
+                    };
+                }
+
+            },
+            {
+                label: "Item Code",
+                fieldname: "item_code",
+                fieldtype: "Link",
+                options: "Item",
+                "get_query": function () {
+                    return {
+                        "filters": {
+                            "disabled": 0,
+                        }
+                    };
+                }
+            },
+            {
+                label: __("Ignore Empty Stock"),
+                fieldname: "ignore_empty_stock",
+                fieldtype: "Check"
+            }
+        ];
+
+        frappe.prompt(fields, function (data) {
+            frappe.call({
+                method: "erpnext.stock.doctype.stock_reconciliation.stock_reconciliation.get_items",
+                args: {
+                    warehouse: data.warehouse,
+                    posting_date: frm.doc.posting_date,
+                    posting_time: frm.doc.posting_time,
+                    company: frm.doc.company,
+                    item_code: data.item_code,
+                    ignore_empty_stock: data.ignore_empty_stock
+                },
+                callback: function (r) {
+                    if (r.exc || !r.message || !r.message.length) return;
+
+                    frm.clear_table("items");
+
+                    r.message.forEach((row) => {
+                        let item = frm.add_child("items");
+                        $.extend(item, row);
+
+                        item.qty = item.qty || 0;
+                        item.valuation_rate = item.valuation_rate || 0;
+                    });
+                    frm.refresh_field("items");
+                }
+            });
+        }, __("Get Items"), __("Update"));
+    },
+})
+
+
+frappe.ui.form.on("Stock Reconciliation Item", {
+
+    uom: function (doc, cdt, cdn) {
+        var d = locals[cdt][cdn];
+        if (d.uom && d.item_code) {
+            return frappe.call({
+                method: "erpnext.stock.doctype.stock_entry.stock_entry.get_uom_details",
+                args: {
+                    item_code: d.item_code,
+                    uom: d.uom,
+                    qty: d.qty
+                },
+                callback: function (r) {
+                    if (r.message) {
+                        frappe.model.set_value(cdt, cdn, r.message);
+                    }
+                }
+            });
+        }
+    },
+
+
+})
