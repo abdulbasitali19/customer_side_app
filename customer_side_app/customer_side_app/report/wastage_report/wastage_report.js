510159d292fb7a366c1a4e8149db6ee908bed481
abdulbasitali19 <abdulbasitalishaik1@gmail.com>
2024-02-20 14:21:51 +0500

    filters added in report

--- customer_side_app/customer_side_app/report/wastage_report/wastage_report.js
+++ customer_side_app/customer_side_app/report/wastage_report/wastage_report.js
@@ -5,5 +5,21 @@
 frappe.query_reports["Wastage Report"] = {
 	"filters": [
 
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.get_today()
+		},
 	]
 };
